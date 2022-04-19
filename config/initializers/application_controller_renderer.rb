8e5fc729c4ca533eb55af7891dbc7cd402c8033a
nagamorikoshi <nagamori329@gmail.com>
2022-04-19 00:06:50 +0000

    ユーザー詳細編集画面追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = '4dd9e89095701e7b2b35403ca0c5330c4f9dfda68553ef8ebe155fb1aba8dc25688580b887627f3630d55f619de8f26f553b34ba48271d85a175e9361a0f2406'
